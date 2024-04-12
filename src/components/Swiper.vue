58d79411a36a9f15c8085666f861bfbd7fc81e46
admrbsn <admrbsn@gmail.com>
2024-04-12 12:00:11 -0600

    mobile

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -17,12 +17,14 @@
           :key="`main-${index}`"
           class="md:w-[768px] flex items-center justify-center cursor-pointer"
           @click="togglePlayPause"
-          @mouseover="() => hoverSwiper = true"
-          @mouseleave="() => hoverSwiper = false"
-          @touchstart="() => hoverSwiper = true"
-          @touchend="() => hoverSwiper = false"
+          @mouseover="() => (hoverSwiper = true)"
+          @mouseleave="() => (hoverSwiper = false)"
+          @touchstart="() => (hoverSwiper = true)"
+          @touchend="() => (hoverSwiper = false)"
         >
-          <div class="w-full h-[300px] md:w-[768px] md:h-[432px] mx-auto rounded overflow-hidden">
+          <div
+            class="w-full h-[300px] md:w-[768px] md:h-[432px] mx-auto rounded overflow-hidden"
+          >
             <template v-if="item.type === 'video'">
               <VideoPlayer
                 :url="item.url"
@@ -56,34 +58,41 @@
       />
     </div>
     <div class="relative">
-    <!-- Thumbnails Swiper -->
-    <swiper-container
-      class="thumbs-swiper w-full md:w-[768px] mt-2"
-      :space-between="8"
-      :free-mode="true"
-      :watch-slides-progress="true"
-      :slides-per-view="3"
-      :breakpoints="{
-        '768': {
-          slidesPerView: 4,
-        },
-        '1024': {
-          slidesPerView: 5,
-        },
-      }"
-    >
-      <swiper-slide
-        v-for="(item, index) in mediaItems"
-        :key="`thumb-${index}`"
-        class="transition-transform transition-opacity duration-100 cursor-pointer"
+      <!-- Thumbnails Swiper -->
+      <swiper-container
+        class="thumbs-swiper w-full md:w-[768px] mt-2"
+        :space-between="8"
+        :free-mode="true"
+        :watch-slides-progress="true"
+        :slides-per-view="3"
+        :breakpoints="{
+          '768': {
+            slidesPerView: 4,
+          },
+          '1024': {
+            slidesPerView: 5,
+          },
+        }"
       >
-        <img :src="item.thumbnail" :alt="`Thumbnail ${index+1}`" class="w-full h-full object-cover rounded overflow-hidden">
-        <div v-if="item.name" class="absolute bottom-0 left-0 w-full h-8 flex items-end pb-1 px-1 text-white text-[10px] font-semibold">
-          <span class="py-1 px-2 bg-black rounded-full">{{ item.name }}</span>
-        </div>
-      </swiper-slide>
-    </swiper-container>
-  </div>
+        <swiper-slide
+          v-for="(item, index) in mediaItems"
+          :key="`thumb-${index}`"
+          class="transition-transform transition-opacity duration-100 cursor-pointer"
+        >
+          <img
+            :src="item.thumbnail"
+            :alt="`Thumbnail ${index + 1}`"
+            class="w-full h-full object-cover rounded overflow-hidden"
+          />
+          <div
+            v-if="item.name"
+            class="absolute bottom-0 left-0 w-full h-8 flex items-end pb-1 px-1 text-white text-[10px] font-semibold"
+          >
+            <span class="py-1 px-2 bg-black rounded-full">{{ item.name }}</span>
+          </div>
+        </swiper-slide>
+      </swiper-container>
+    </div>
   </div>
 </template>
 
@@ -155,7 +164,6 @@ onMounted(() => {
 .swiper-container::part(button-prev) {
   @apply left-[calc(50%-448px)] w-4 opacity-25 hover:opacity-100;
 }
-
 
 .swiper-container::part(button-next) {
   @apply right-[calc(50%-448px)] w-4 opacity-25 hover:opacity-100;
